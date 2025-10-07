dd63cf518a62df144197e9d0777a57961f516521
Angeles <maria.fi1701@gmail.com>
2025-10-07 09:36:15 -0500

    Se creo el componente de recetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';  // Si tienes un HomeComponent
+import { RecetasComponent } from './recetas/recetas.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige a home por defecto
   { path: 'home', component: HomeComponent, title:'Home' },          // Ruta para la página de inicio
   { path: 'login', component: LoginComponent, title:'Login'},        // Ruta para Login
   { path: 'signup', component: LoginComponent, title:'Registro' },       // Ruta para Registro (en el mismo componente de Login)
+  { path: 'recetas', component: RecetasComponent, title:'Recetas' },             // Ruta para Recetas
 ];
 
 @NgModule({
